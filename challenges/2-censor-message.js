14939f1aa2c9f851df00720bd3e2fe427d054c77
Leo <leo.maedje@gmail.com>
2022-06-30 17:52:50 +0100

    All tests are now passed by censorMessage

--- challenges/2-censor-message.js
+++ challenges/2-censor-message.js
@@ -8,25 +8,22 @@ You may assume that there is no punctuation apart from spaces in the full messag
 */
 
 function censorMessage(message, banList) {
-    let rawWords = 
+    let rawWords = message.split(" ");
+    let scrubbedWords = [];
+    for (let word of rawWords) {
+        let lowerCaseWord = word.toLowerCase();
+        if (banList.includes(lowerCaseWord)) {
+            scrubbedWords.push("*****");
+        } else {
+            scrubbedWords.push(word);
+        }
+    }
+    let scrubbedMessage = scrubbedWords.join(" ");
+    return scrubbedMessage;
 }
-set RAW_WORDS to the list of strings created by separating the string message into sub-strings at every space (' ')
-
-set SCRUBBED_WORDS to an empty list
-
-for each string, WORD, in RAW_WORDS
-	set LOWERCASE_WORD to lowercase(word)
-  if LOWERCASE_WORD is contained in the list BAN_LIST
-    insert the string '*****' at the end of SCRUBBED_WORDS
-  else
-    insert WORD at the end of SCRUBBED_WORDS
-
-set SCRUBBED_MESSAGE to the string created by joining together every string in SCRUBBED_WORDS with a space (' ') as a join character
-
-return SCRUBBED_MESSAGE
 
 //test cases:
 //===========
-console.log('censorMessage("don't mess with cats", ["mess"])',censorMessage("don't mess with cats", ["mess"]) ,'should be "don't ***** with cats"')
-console.log('censorMessage("Are you sure that this is safe", ["are", "is"]) ',censorMessage("Are you sure that this is safe", ["are", "is"]) ,'should be "***** you sure that this ***** safe"')
-console.log('censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"])',censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]) ,'should be "I CANNOT ***** *****"')
+console.log(`censorMessage( "don't mess with cats", ["mess"])`, censorMessage("don't mess with cats", ["mess"]) ,`should be "don't ***** with cats"`)
+console.log('censorMessage("Are you sure that this is safe", ["are", "is"]) ', censorMessage("Are you sure that this is safe", ["are", "is"]) ,'should be "***** you sure that this ***** safe"')
+console.log('censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"])', censorMessage("I CANNOT HELP YOU", ["can", "not", "help", "you"]) ,'should be "I CANNOT ***** *****"')
