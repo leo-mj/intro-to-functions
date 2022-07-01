28b1b8fa9108ea8a4042b28f1df7310793644574
Leo <leo.maedje@gmail.com>
2022-07-01 11:57:02 +0100

    Added JS Doc to 0-check-if-equal.js

--- challenges/0-check-if-equal.js
+++ challenges/0-check-if-equal.js
@@ -1,7 +1,12 @@
 /* Write a function that, given two numbers, returns a boolean representing whether or not the two numbers are equal.
 You may assume that the numbers are always integers (i.e. whole numbers).
 */
-
+/**
+ * Given two numbers, returns a boolean representing whether or not the two numbers are equal.
+ * @param {number} numOne - first number in comparison 
+ * @param {*} numTwo - second nnumber in comparison 
+ * @returns {boolean} - whether the first and second number are equal
+ */
 function isEqual(numOne, numTwo) {
     if (numOne === numTwo) {
         return true;
